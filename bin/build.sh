29801fb7085e134fa2c8e63b9fbc088386d17cf3
GoForBroke1006 <go.for.broke1006@gmail.com>
2018-07-07 16:58:42 +0300

    Add build files

--- bin/build.sh
+++ bin/build.sh
@@ -3,3 +3,9 @@
 mvn clean package
 cp target/prostokvashino-acceptance-1.0-SNAPSHOT-fat-tests.jar \
     ./build/prostokvashino-acceptance.jar
+
+zip ./build/prostokvashino-acceptance-build.zip \
+    .env.dist \
+    prostokvashino-acceptance.jar \
+    run.bat \
+    run.sh
